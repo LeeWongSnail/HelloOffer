6eb10c24f27e4f7524efe66d9a436c4ae81fd2b3
LeeWongSnail <wangli_0632@163.com>
2018-03-31 17:32:17 +0800

    add permutation

---
+++ Permutation/Permutation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Permutation
+//
+//  Created by LeeWong on 2018/3/31.
+//  Copyright © 2018年 LeeWong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
